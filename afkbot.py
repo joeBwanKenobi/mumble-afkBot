bb6ecf6bed9ae9a8ff4be3b21c9636914d4438fb
Joe Flanagan <josephbflanagan@gmail.com>
2019-02-27 12:53:36 -0500

    updated afkbot.py

--- afkbot.py
+++ afkbot.py
@@ -1,4 +1,5 @@
 import pymumble_py3 as pymumble
+from time import sleep
 
 afkbot = pymumble.Mumble("multiverse.jo3.io", "afkBot", password='beersh!tz')
 afkbot.set_application_string('afkbot')
@@ -8,7 +9,18 @@ afkbot.set_bandwidth(72000)
 
 userList = afkbot.users
 
-print(userList)
-for session in userList:
-    print(userList[session].get_property('name'))
-    userList[session].send_message('HEY! This is a test message')
+
+isRunning = True;
+
+while isRunning:
+    for session in userList:
+        print(userList)
+        for u in userList:
+            print(userList[u]['name']+ ' sound: ' + str(userList[u].sound.is_sound()))
+            userList[u].mute()
+
+    sleep(10)
+#         # print(userList[session].get_property('name'))
+#         # userList[session].send_message('HEY! You will be muted now, this is a test.')
+#         print(userList[session].mute())
+#         print(userList[session].idlesecs)
